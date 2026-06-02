e962e3d75b91a5f93efdf0b6ff4e2dd668d51e09
ajay <ajayjarwal44@gmail.com>
2026-06-02 15:05:23 +0530

    add models and migrations for post and category

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'published',
+        'user_id',
+        'category_id',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
