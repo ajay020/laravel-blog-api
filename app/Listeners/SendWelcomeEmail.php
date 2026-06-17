a404d01462d4461009a082cb37d4e511d20bfd1e
ajay <ajayjarwal44@gmail.com>
2026-06-17 13:27:40 +0530

    add welcome email functionality

--- app/Listeners/SendWelcomeEmail.php
+++ app/Listeners/SendWelcomeEmail.php
@@ -3,11 +3,13 @@
 namespace App\Listeners;
 
 use App\Events\UserRegistered;
+use App\Mail\WelcomeMail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
-class SendWelcomeEmail  implements ShouldQueue 
+class SendWelcomeEmail implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -23,7 +25,10 @@ class SendWelcomeEmail  implements ShouldQueue
     public function handle(UserRegistered $event): void
     {
         try {
-            Log::info('Welcome email sent to: ' . $event->user->email);
+             Mail::to($event->user->email)
+                    ->send(new WelcomeMail($event->user));
+
+            // Log::info('Welcome email sent to: ' . $event->user->email);
         } catch (\Throwable $e) {
             Log::error('Failed to send welcome email: ' . $e->getMessage());
         }
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+
+    <h2 class="text-2xl font-bold text-yellow-400">Welcome, {{ $user->name }}!</h2>
+
+    <p class="text-muted  text-green-400 text-2xl">
+        Thank you for registering. 😊
+    </p>
+
+    <p>We're excited to have you with us.</p>
+
+</body>
+</html>
